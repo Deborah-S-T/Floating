867439f4cca4f5da6c1e743fe704e93ba915e78b
Deborah-S-T <deborahsolomon04@gmail.com>
2025-12-22 19:48:27 +0100

    new :: feat :: player_input mini propulsions

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,19 +4,22 @@ using UnityEngine.InputSystem;
 public class Example : MonoBehaviour
 {
     InputAction moveAction;
+    InputAction propulsionAction;
 
     public float rotationSpeed = 10.0f;
     public float speed = 5.0f;
+    public float propulsionForce = 5.0f;
 
     private Rigidbody rb;
 
     private void Start()
     {
         moveAction = InputSystem.actions.FindAction("Move");
+        propulsionAction = InputSystem.actions.FindAction("Jump");
         rb = GetComponent<Rigidbody>();
     }
 
-    void FixedUpdate()
+    void Update()
     {
         Vector2 moveValue = moveAction.ReadValue<Vector2>();
 
@@ -27,6 +30,13 @@ public class Example : MonoBehaviour
         float pitchAmount = moveValue.y * rotationSpeed * Time.fixedDeltaTime;
         Quaternion pitchRotation = Quaternion.Euler(pitchAmount, 0f, 0f);
         rb.MoveRotation(rb.rotation * pitchRotation);
+
+        if (propulsionAction.triggered)
+        {
+
+            rb.MovePosition(rb.position + propulsionForce * transform.up * Time.fixedDeltaTime);
+
+        }
 
     }
 }
