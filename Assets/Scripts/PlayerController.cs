4ec910ab5c20314d509e246eae8f45699610ab91
Deborah-S-T <deborahsolomon04@gmail.com>
2025-12-25 21:07:49 +0100

    new : boosters

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,20 +7,16 @@ public class Example : MonoBehaviour
     // inputs
     InputAction moveAction;
     InputAction propulsionAction;
-    InputAction stickAction;
 
     // movement variables
     public float rotationSpeed = 10.0f;
     public float speed = 5.0f;
 
-    public float minipropulsionForce = 1.0f;
-    public float properPropulsionForce = 50.0f;
+    private static float minipropulsionForce = 50.0f;
+    private static float properPropulsionForce = 500.0f;
+    private float availablePropulsions = 4 * properPropulsionForce;
 
     Vector2 moveValue;
-
-    // sticking
-    private bool holding = false;
-    Vector3 contactNormal;
 
     // rigidbody
     private Rigidbody rb;
@@ -31,7 +27,6 @@ public class Example : MonoBehaviour
         // get input actions
         moveAction = InputSystem.actions.FindAction("Move");
         propulsionAction = InputSystem.actions.FindAction("Jump");
-        stickAction = InputSystem.actions.FindAction("Interact");
 
         rb = GetComponent<Rigidbody>();
     }
@@ -50,76 +45,42 @@ public class Example : MonoBehaviour
         MovePlayer();
     }
 
-    // ------------------------COLLISION DETECTION-----------------------
-    void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Wall"))
-        {
-            if (!stickAction.IsPressed())
-            {
-                // if collided with a wall, propel the player away from it
-                Vector3 wallNormal = collision.contacts[0].normal;
-                Propulsion(properPropulsionForce, wallNormal);
-            }
-
-            contactNormal = collision.contacts[0].normal;
-            Vector3 contactPoint = collision.contacts[0].point;
-            float normalOffset = Vector3.Dot(transform.position - contactPoint, contactNormal);
-            normalOffset = Mathf.Max(normalOffset, 0.1f);
-            Stick(contactNormal, contactPoint, normalOffset);
-        }
-    }
-    /*
+    // ------------------------BOOSTER DETECTION-----------------------
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Wall"))
+        Transform parent = other.transform.transform.parent;
+        if (parent.CompareTag("Booster"))
         {
-            if (!stickAction.IsPressed())
-            {
-                // if collided with a wall, propel the player away from it
-                Vector3 wallNormal = (transform.position - other.ClosestPoint(transform.position)).normalized;
-                Propulsion(properPropulsionForce, wallNormal);
-            }
-
-            contactNormal = (transform.position - other.ClosestPoint(transform.position)).normalized;
-            Vector3 contactPoint = other.ClosestPoint(transform.position);
-            float normalOffset = Vector3.Dot(transform.position - contactPoint, contactNormal);
-            normalOffset = Mathf.Max(normalOffset, 0.1f);
-            Stick(contactNormal, contactPoint, normalOffset);
+            availablePropulsions += 2 * properPropulsionForce;
+            Destroy(other.gameObject);
         }
-    }*/
+    }
 
     // ----------------------------------------------------------------------------------------------------
 
     // -----------------------------MOVE PLAYER-------------------------
     void MovePlayer()
     {
-        if (!holding)
+        // rotate forword/backward and sidways
+        float yawAmount = moveValue.x * rotationSpeed * Time.fixedDeltaTime;
+        float pitchAmount = moveValue.y * rotationSpeed * Time.fixedDeltaTime;
+
+        // apply the rotation
+        Quaternion Rotation = Quaternion.Euler(pitchAmount, yawAmount, 0f);
+        rb.MoveRotation(rb.rotation * Rotation);
+
+        // apply mini propulsion upward
+        if (propulsionAction.triggered)
         {
-            // rotate forword/backward and sidways
-            float yawAmount = moveValue.x * rotationSpeed * Time.fixedDeltaTime;
-            float pitchAmount = moveValue.y * rotationSpeed * Time.fixedDeltaTime;
-
-            // apply the rotation
-            Quaternion Rotation = Quaternion.Euler(pitchAmount, yawAmount, 0f);
-            rb.MoveRotation(rb.rotation * Rotation);
-
-            // apply mini propulsion upward
-            if (propulsionAction.IsPressed())
+            if (availablePropulsions >= 0)
+            {
+                Propulsion(properPropulsionForce, transform.up);
+                availablePropulsions -= properPropulsionForce;
+            }
+            else
             {
                 Propulsion(minipropulsionForce, transform.up);
             }
-        }
-        else
-        {
-            float forward_backMovement = moveValue.y * speed * Time.fixedDeltaTime;
-            float sideMovement = moveValue.x * speed * Time.fixedDeltaTime;
-
-            // apply movement
-            Vector3 moveDir = Vector3.ProjectOnPlane(transform.up, contactNormal).normalized * forward_backMovement
-                + Vector3.ProjectOnPlane(transform.right, contactNormal).normalized * sideMovement;
-
-            transform.position += moveDir;
         }
     }
 
@@ -130,35 +91,4 @@ public class Example : MonoBehaviour
         rb.AddForce(direction * propForce, ForceMode.Acceleration);
     }
 
-    // -----------------------------STICK--------------------------------
-    void Stick(Vector3 normal, Vector3 point, float offset)
-    {
-        if (stickAction.IsPressed())
-        {
-            // sticks to the surface it touches
-            transform.position = point + offset;
-            holding = true;
-        }
-        else
-        {
-            holding = false;
-        }
-        /*
-        if (stickAction.IsPressed())
-        {
-            // stop physics from moving the player
-            rb.linearVelocity = Vector3.zero;
-            rb.isKinematic = true;
-
-            // snap player slightly above the surface
-            transform.position = point + normal * offset;
-            holding = true;
-        }
-        else
-        {
-            // release stick
-            rb.isKinematic = false;
-            holding = false;
-        }*/
-    }
 }
